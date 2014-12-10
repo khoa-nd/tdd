fc2154b582c2656f18a81abd9d1fba069b8c9d15
khoa-nd <ndkhoa.is@gmail.com>
2014-12-10 23:35:53 +0700

    Second Test: Multiple Subscriber Receives Message

--- src/main/java/RaceResultsService.java
+++ src/main/java/RaceResultsService.java
@@ -1,15 +1,20 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
 /**
  * Created by NguyenDangKhoa on 12/6/14.
  */
 public class RaceResultsService {
 
-    private Client client;
+    private Collection<Client> clients = new ArrayList<Client>();
 
     public void addSubscriber(Client client) {
-        this.client = client;
+        clients.add(client);
     }
 
     public void send(Message message) {
-        this.client.receive(message);
+        for (Client client : clients) {
+            client.receive(message);
+        }
     }
 }
--- src/test/java/RaceResultsServiceTest.java
+++ src/test/java/RaceResultsServiceTest.java
@@ -1,19 +1,30 @@
 import org.junit.Test;
-import org.mockito.Mockito;
 
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class RaceResultsServiceTest {
 
+    private RaceResultsService raceResultsService = new RaceResultsService();
+    private Client clientA = mock(Client.class);
+    private Client clientB = mock(Client.class);
+    private Message message = mock(Message.class);
+
     @Test
     public void subscribedClientShouldReceiveMessage() {
-        RaceResultsService raceResultsService = new RaceResultsService();
-        Client client = mock(Client.class);
-        Message message = mock(Message.class);
-
-        raceResultsService.addSubscriber(client);
+        raceResultsService.addSubscriber(clientA);
         raceResultsService.send(message);
 
-        verify(client).receive(message);
+        verify(clientA).receive(message);
+    }
+
+    @Test
+    public void messageShouldBeSentToAllSubscribedClients() {
+        raceResultsService.addSubscriber(clientA);
+        raceResultsService.addSubscriber(clientB);
+        raceResultsService.send(message);
+
+        verify(clientA).receive(message);
+        verify(clientB).receive(message);
     }
 }

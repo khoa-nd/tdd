4190e8b7cfb0d8997b786fa5ed0a0dd06c5b9c05
khoa-nd <ndkhoa.is@gmail.com>
2014-12-14 19:45:37 +0700

    The Third Test: Send Messages to Subscribers Only

--- src/test/java/RaceResultsServiceTest.java
+++ src/test/java/RaceResultsServiceTest.java
@@ -1,6 +1,7 @@
 import org.junit.Test;
 
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
 
 public class RaceResultsServiceTest {
@@ -10,6 +11,16 @@ public class RaceResultsServiceTest {
     private Client clientB = mock(Client.class);
     private Message message = mock(Message.class);
 
+    // zero subscribers
+    @Test
+    public void notSubscribedClientShouldNotReceiveMessage() {
+        raceResultsService.send(message);
+
+        verify(clientA, never()).receive(message);
+        verify(clientB, never()).receive(message);
+    }
+
+    // one subscriber
     @Test
     public void subscribedClientShouldReceiveMessage() {
         raceResultsService.addSubscriber(clientA);
@@ -18,6 +29,7 @@ public class RaceResultsServiceTest {
         verify(clientA).receive(message);
     }
 
+    // two subscribers
     @Test
     public void messageShouldBeSentToAllSubscribedClients() {
         raceResultsService.addSubscriber(clientA);
